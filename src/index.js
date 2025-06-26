fd90150d0a3a57c801f2b4288362f426a6884534
denilsontorres <dtlima2020@gmail.com>
2025-06-26 10:21:16 -0300

    :tada: First-commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const player1 = {
+  PERSONAGEM: "Mario",
+  VELOCIDADE: 4,
+  MANOBRABILIDADE: 3,
+  PODER: 3,
+  PONTOS: 0,
+};
+const player2 = {
+  PERSONAGEM: "Luigi",
+  VELOCIDADE: 3,
+  MANOBRABILIDADE: 4,
+  PODER: 4,
+  PONTOS: 0,
+};
